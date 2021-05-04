d6178c714b688850cc7cc4e81100675041b77da3
Lucas de Souza Lisack <lucasdesouzalisack@gmail.com>
2021-05-03 21:57:21 -0300

    Projetos Criador

---
+++ backend/src/test/java/com/devesuperior/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
