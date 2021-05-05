d3214cb1c5a501f0530d665d9568ee14a2ccdd5e
lzkz <lucasdesouzalisack@gmail.com>
2021-05-05 11:53:03 -0300

    Layers

---
+++ backend/src/main/java/com/devesuperior/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devesuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devesuperior.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
